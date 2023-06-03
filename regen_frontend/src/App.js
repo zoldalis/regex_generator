6ba9876a1c05fa89045d866f2350c9596f1fa665
Михаил Дудко <mikel2000@bk.ru>
2023-06-03 21:09:36 +0500

    commit before pushing to github

--- regen_frontend/src/App.js
+++ regen_frontend/src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
 
+/* var InputText = React.createClass({
+  getInitialState:function(){
+     return {
+        textVal:''
+     }
+ },
+  render: function () {
+    return (
+      <input type="text" value={this.state.textVal} autofocus="autofocus" onChange={this.handleChange} />
+    )
+  },
+  handleChange: function (evt) {
+    this.setState({ textVal: evt.target.value.substr(0, 100) });
+  }
+}); */
+
+function generate_output(input_text)
+{
+
+}
+
+const generate_clicked = event => {
+  // 👇️ access textarea value
+  const input = document.getElementById("input_text");
+  let text = input.value;
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>REgular Expressions GENerator</h1>
       </header>
+      <div className="content-center">
       <div className="sidebar">
         <ul>
           <li>Home</li>
@@ -15,12 +43,26 @@ function App() {
         </ul>
       </div>
       <div className="main-content">
-        <textarea className="text-edit" rows="10"></textarea>
-        <textarea className="text-edit" rows="10"></textarea>
-        <button className="button">Generate</button>
+        <textarea 
+        id="input_text"
+        name="input_text"
+        value={""}
+        className="text-edit" rows="10"></textarea>
+        <textarea 
+        id="ouput_text"
+        name="ouput_text"
+        value={""}
+        className="text-edit" rows="10"></textarea>
+        <button 
+        id="generate_button"
+        onClick={generate_clicked}
+        className="button">Generate</button>
       </div>
+    </div>
     </div>
   );
 }
+
+
 
 export default App;
